9431151b24a8591ac5641eb2a1a17362417ae4b7
AlexCapoen <alex.capoen@isen.yncrea.fr>
2020-10-07 19:19:16 +0200

    add pop up menu

--- header.php
+++ header.php
@@ -1,15 +1,20 @@
-<header> 
+<header>
   	<div id='headerContainer'>
   		<div id="headerContent">
 			<h1 id="headerTitle">Welcome at Quizz.com</h1>
 			<br/>
 			<div id="linkContainer">
 				<a class="navlink" href="main.php">Home</a>
-				<a class="navlink" href="quizz1.php">Quizz</a>
-				
+        <div id="quizz">
+				<a class="navlink">Quizz</a>
+          <div class="navQuizz">
+            <a class="quizzElement" href="./quizz1.php">Quizz 1</a>
+            <a class="quizzElement" href="./quizz2.php">Quizz 2</a>
+          </div>
+        </div>
 			</div>
 		</div>
 		<a class="navlink" id="loginLink" href='login.php'><i class="fas fa-user-circle"></i>Login</a>
 	</div>
-	
+
 </header>
