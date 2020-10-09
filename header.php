e9348709f96ccd4e2c59b225ccccb6414fd9869a
ERAUD-BERTHAUD <hugo.eraud-berthaud@isen.yncrea.fr>
2020-10-09 14:39:44 +0200

    Auto stash before merge of "mock_up" and "origin/mock_up"
    bug home corrected

--- header.php
+++ header.php
@@ -1,17 +1,19 @@
 <header>
-  	<div id='headerContainer'>
-  		<div id="headerContent">
+	<div id='headerContainer'>
+		<div id="headerContent">
 			<h1 id="headerTitle">Welcome at Quizz.com</h1>
 			<br/>
 			<div id="linkContainer">
-				<a class="navlink" href="main.php">Home</a>
-        <div id="quizz">
-				<a class="navlink">Quizz</a>
-          <div class="navQuizz">
-            <a class="quizzElement" href="./quizz1.php">Quizz 1</a>
-            <a class="quizzElement" href="./quizz2.php">Quizz 2</a>
-          </div>
-        </div>
+				<div>
+					<a class="navlink" id="linkHomeHeader" href="main.php">Home</a>
+				</div>
+				<div id="quizz">
+					<a class="navlink" id="linkQuizzHeader">Quizz</a>
+					<div class="navQuizz">
+						<a class="quizzElement" href="./quizz1.php">Quizz 1</a>
+						<a class="quizzElement" href="./quizz2.php">Quizz 2</a>
+					</div>
+				</div>
 			</div>
 		</div>
 		<a class="navlink" id="loginLink" href='login.php'><i class="fas fa-user-circle"></i>Login</a>
