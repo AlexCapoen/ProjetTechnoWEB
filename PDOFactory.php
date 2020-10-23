316f2f3bcd98614587c4ff4ae8f1b32b1ab102ea
Johan ROUX <johanroux@isen.yncrea.fr>
2020-10-23 18:04:08 +0200

    ready to work

--- PDOFactory.php
+++ PDOFactory.php
@@ -3,7 +3,7 @@
 		private static $connexion;
 			public static function get(){
 			   if(!self::$connexion instanceof PDO){
-			      self::$connexion = new PDO('mysql:host=localhost;dbname=quizz;charset=utf8', 'root', '');
+			      self::$connexion = new PDO('mysql:host=localhost;dbname=db;charset=utf8', 'root', '');
 			   }
 			   return self::$connexion;
 			}
